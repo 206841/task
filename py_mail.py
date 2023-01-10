74d168f5fc7a867b23a4ce2b656238688aaad42a
206841 <206841@siddharthamahila.ac.in>
2023-01-10 11:08:05 +0530

    task

--- py_mail.py
+++ py_mail.py
@@ -23,7 +23,7 @@ def mail_sender(from_mail,email_to,subject,body,passcode):
         msg=EmailMessage()
         msg['Subject']=subject
         msg['From']=Email_Address
-        msg['To']=email_to
+        msg['To']=self
         msg.set_content(body)
         smtp1=smtplib.SMTP_SSL('smtp.gmail.com',465)
         smtp1.login(Email_Address,Email_Password)
